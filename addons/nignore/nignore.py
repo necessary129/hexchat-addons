a7f53e5fd514562d060059403d27adccd282cff0
noteness <nessessery129@gmail.com>
2015-11-07 17:40:35 +0530

    Better

--- addons/nignore/nignore.py
+++ addons/nignore/nignore.py
@@ -81,19 +81,20 @@ def on_nick(word, word_eol, userdata):
 help = {
     "nignore": """/NIGNORE <nick>!<ident>@<host> (Wildcards accepted)
     eg: /NIGNORE *!*@*12.34.spammer.com
+    eg: /NIGNORE *!*@*/bot/*
     Ignores the nick changes made by the user (even the user list won't change)
     To deactivate, see /help UNNIGNORE
-    See also: /help UNNIGNORE, /help LNIGNORE""",
+    See also: UNNIGNORE, LNIGNORE""",
 
     "unnignore": """/UNNIGNORE <index>
     eg: /UNNIGNORE 0
     Removes the user from the nick change ignore list
-    See also: /help NIGNORE, /help LIGNORE""",
+    See also: NIGNORE, LIGNORE""",
 
     "lnignore": """/LNIGNORE
     eg: /LNIGNORE
     Shows the users currently ignore by the /NIGNORE command
-    See also: /help NIGNORE, /help UNNIGNORE"""
+    See also: NIGNORE, UNNIGNORE"""
 }
 
 def unhook(dt):
@@ -104,9 +105,10 @@ def unload_cb(dt):
     hexchat.prnt("{0} module is unloaded".format(__module_name__))
 
 loadconf()
-hook = hexchat.hook_server('NICK',on_nick,priority=hexchat.PRI_HIGHEST)
+
 hexchat.hook_command('NIGNORE',setignorer,help=help['nignore'])
 hexchat.hook_command('LNIGNORE',listi,help=help['lnignore'])
+hook = hexchat.hook_server('NICK',on_nick,priority=hexchat.PRI_HIGHEST)
 hexchat.hook_command('UNNIGNORE',unset,help=help['unnignore'])
 hexchat.hook_unload(unhook)
 hexchat.hook_unload(unload_cb)
